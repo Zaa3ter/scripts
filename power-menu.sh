bfdd0d7cff37ef678bfcac5fc4b46ce945465d92
falajh <almogtbanmr123@gmail.com>
2026-04-01 21:43:54 +0300

    format

--- power-menu.sh
+++ power-menu.sh
@@ -4,12 +4,12 @@
 #export XAUTHORITY=/run/lightdm/zaater/xauthority
 
 
-choice=$(printf "Cancel\n  Shutdown\n󰑓  Reboot\n󰒲  Sleep\n󰗽  Logout" | rofi -dmenu -theme /home/zaater/.config/rofi/dmenu.rasi -theme-str 'window { width: 30%; }' -i -p "Power Menu")
+choice=$(printf "Cancel\n \tShutdown\n󰑓 \tReboot\n󰒲 \tSuspend\n󰗽 \tLogout" | rofi -dmenu -theme /home/zaater/.config/rofi/dmenu.rasi -theme-str 'window { width: 30%; }' -i -p "Power Menu")
 
 case "$choice" in
   *Shutdown) systemctl poweroff ;;
   *Reboot) systemctl reboot ;;
-  *Sleep) systemctl suspend && i3lock ;;
+  *Suspend) systemctl suspend ;;
   *Logout) i3-msg exit ;;
   *) exit 0 ;;
 esac
